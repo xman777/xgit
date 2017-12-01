cf8d4722143ad33526b3787b67c116468c8741b8
xman777 <xu583545339@163.com>
2017-12-01 10:20:36 +0800

    common注释添加
    给common包下的文件添加注释，并记录到项目日志中

--- adamant/src/com/adamant/common/XFilter.java
+++ adamant/src/com/adamant/common/XFilter.java
@@ -13,6 +13,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+/**
+ * 过滤器demo
+ * TODO: 
+ * 1、Filter简介
+ * 		Filter也称之为过滤器，它是Servlet技术中最实用的技术，Web开发人员通过Filter技术，对web服务器管理的所有web资源：例如Jsp, Servlet, 静态图片文件或静态 html 文件等进行拦截，
+ *		从而实现一些特殊的功能。例如实现URL级别的权限访问控制、过滤敏感词汇、压缩响应信息等一些高级功能。
+ *		它主要用于对用户请求进行预处理，也可以对HttpServletResponse进行后处理。使用Filter的完整流程：Filter对用户请求进行预处理，接着将请求交给Servlet进行处理并生成响应，最后Filter再对服务器响应进行后处理。
+ * 2、Filter功能
+ *		在HttpServletRequest到达 Servlet 之前，拦截客户的HttpServletRequest 。根据需要检查HttpServletRequest，也可以修改HttpServletRequest头和数据。
+ *		在HttpServletResponse到达客户端之前，拦截HttpServletResponse 。根据需要检查HttpServletResponse，也可以修改HttpServletResponse头和数据。
+ * 3、如何借助Filter实现拦截功能
+ * 		Filter接口中有一个doFilter方法，当开发人员编写好Filter，并配置对哪个web资源进行拦截后，Web服务器每次在调用web资源的service方法之前，都会先调用一下filter的doFilter方法，因此，在该方法内编写代码可达到如下目的：
+ *		调用目标资源之前，让一段代码执行。是否调用目标资源（即是否让用户访问web资源）。
+ *		web服务器在调用doFilter方法时，会传递一个filterChain对象进来，filterChain对象是filter接口中最重要的一个对象，它也提供了一个doFilter方法，开发人员可以根据需求决定是否调用此方法，调用该方法，则web服务器就会调用web资源的service方法，即web资源就会被访问，否则web资源不会被访问。
+ * @author xman
+ * @time 2017年10月1日 上午10:02:33
+ * @version v1.0
+ */
 public class XFilter implements Filter {
 	private FilterConfig config; //FilterConfig可以获取部署描述符文件（web.xml）中分配的过滤器初始化参数
 	
